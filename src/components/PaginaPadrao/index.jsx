ce097a9b41992cde76f1fd902ee237f6279c652a
Luiza Valentini <luizadaravalentini@gmail.com>
2022-11-22 00:01:59 -0300

    arrumando importaçoes

--- src/components/PaginaPadrao/index.jsx
+++ src/components/PaginaPadrao/index.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import styles from './PaginaPadrao.module.css';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../Navbar';
 import Header from '../Header';
 import Footer from '../Footer';
 import Banner from '../Banner';
