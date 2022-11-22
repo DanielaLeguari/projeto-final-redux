b822d392a73da48688d8bc64ad92a14aedf33861
Luiza Valentini <luizadaravalentini@gmail.com>
2022-11-22 01:34:24 -0300

    adicionando favicon

---
+++ src/pages/Carrinho/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Home.module.css';
+import categorias from '../../json/categoria.json';
+import { Link } from 'react-router-dom';
+
+
+const Home = () => {
+  return (
+ <>
+ </>
+  )
+}
+
+export default Home;
