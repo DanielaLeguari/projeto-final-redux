76c9a1c496e534c025e73595205547b6be4d06d7
Daniela Leguari <danielaleguari@outlook.com>
2022-11-22 23:42:35 -0300

    iniciando refatoração de estilo page categoria

--- src/components/CardColecionavel/index.jsx
+++ src/components/CardColecionavel/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './CardColecionavel.module.css';
 import { useDispatch } from 'react-redux';
-import { AiOutlineHeart, AiOutlineShoppingCart, AiFillHeart  } from 'react-icons/ai';
+import { AiOutlineHeart, AiOutlineShoppingCart, AiFillHeart } from 'react-icons/ai';
 import { mudarFavorito } from 'store/reducers/itens';
 
 const CardColecionavel = ({ titulo, descricao, foto, favorito, preco, id, categoria }) => {
@@ -13,19 +13,24 @@ const CardColecionavel = ({ titulo, descricao, foto, favorito, preco, id, catego
   }
 
   return (
-    <div className={styles.item}>
-      <p className={styles.titulo}>{titulo}</p>
-      <img src={foto} alt=""></img>
-      <p className={styles.descricao}>Sobre o item: {descricao}</p>
-      <div className={styles.containerDetalhes}>
-        <div className={styles.containerPreco}>
-          <p className={styles.valor}>R${preco}</p>
+    <>
+      <div className={styles.post}>
+        <div className={styles.item}>
+          <p className={styles.titulo}>{titulo}</p>
+          <img className={styles.imagemItens} src={foto} alt=""></img>
+          <p className={styles.descricao}>Sobre o item: {descricao}</p>
+          
         </div>
-        {favorito ? <AiFillHeart color='#041833' onClick={resolverFavorito} size={'2rem'} />
-          : <AiOutlineHeart  size={'2rem'} onClick={resolverFavorito} />}
-        <AiOutlineShoppingCart size={'2rem'} />
+        <div className={styles.containerDetalhes}>
+            <div className={styles.containerPreco}>
+              <p className={styles.valor}>R${preco}</p>
+            </div>
+            {favorito ? <AiFillHeart color='red' onClick={resolverFavorito} size={'2rem'} />
+              : <AiOutlineHeart size={'2rem'} color='white' onClick={resolverFavorito} />}
+            <AiOutlineShoppingCart size={'2rem'} color='white'  />
+          </div>
       </div>
-    </div>
+    </>
   );
 }
 
